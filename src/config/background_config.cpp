68f5880fc410d99f88d6419070ecba979fd0f14c
schonmann <antonio_schonmann@hotmail.com>
2017-06-30 15:18:08 -0700

    Background speeds adjusted;

--- src/config/background_config.cpp
+++ src/config/background_config.cpp
@@ -1,5 +1,5 @@
 #include <background_config.h>
 
 namespace backgroundConfig {
-    double LAYER_DX_MULTIPLIER[NUM_LAYERS] = { 0.3, 0.35, 0.4, 0.6};
+    double LAYER_DX_MULTIPLIER[NUM_LAYERS] = { -0.3, -0.35, -0.4, -0.6 };
 }
