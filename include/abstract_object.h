a0976a6e5866645f71ac8bba6d51e230fd8db5a3
schonmann <antonio_schonmann@hotmail.com>
2017-07-01 12:22:13 -0700

    Formatting.

--- include/abstract_object.h
+++ include/abstract_object.h
@@ -40,59 +40,65 @@ namespace model {
 
             virtual void draw(core::Renderer *renderer);
             virtual void update(float dt);
+            
             virtual SDL_Texture * getTexture();
+
             virtual SDL_Rect * getSrcRect();
             virtual SDL_Rect * getDestRect();
+
             virtual AbstractObject * loadTexture(char * path, int rows, int cols);
             virtual AbstractObject * loadTexture(SDL_Surface * surface, int rows, int cols);
+
             virtual AbstractObject * setLayer(int layer);
             virtual int getLayer();
 
-			virtual AbstractObject * setX(double x);
-            virtual double getX();
-
             virtual AbstractObject * addX(double x);
-            virtual AbstractObject * addDX(double x);
-            virtual AbstractObject * addDDX(double x);
-            virtual AbstractObject * addY(double x);
-            virtual AbstractObject * addDY(double x);
-            virtual AbstractObject * addDDY(double x);
+            virtual double getX();
 
             virtual AbstractObject * setS(double s);
             
+			virtual AbstractObject * setX(double x);
 			virtual AbstractObject * setSX(double sx);
             virtual double getSX();
 
 			virtual AbstractObject * setSY(double sy);
             virtual double getSY();
 
+            virtual AbstractObject * addY(double x);
 			virtual AbstractObject * setY(double y);
             virtual double getY();
 
+            virtual AbstractObject * addDX(double x);
 			virtual AbstractObject * setDX(double dx);
             virtual double getDX();
 
+            virtual AbstractObject * addDDX(double x);
 			virtual AbstractObject * setDDX(double ddx);
             virtual double getDDX();
 
+            virtual AbstractObject * addDY(double x);
 			virtual AbstractObject * setDY(double dy);
             virtual double getDY();
 
+            virtual AbstractObject * addDDY(double x);
 			virtual AbstractObject * setDDY(double ddy);
             virtual double getDDY();
 
 			virtual AbstractObject * setWidth(double w);
             virtual double getWidth();
+
 			virtual AbstractObject * setHeight(double h);
             virtual double getHeight();
 
             virtual AbstractObject * setBoundsX(double a, double b);
             virtual AbstractObject * setBoundsY(double a, double b);
+
             virtual Vector2 getBoundsX();
             virtual Vector2 getBoundsY();
 
             virtual AbstractObject * setBoundsDX(double a, double b);
             virtual AbstractObject * setBoundsDY(double a, double b);
+
             virtual Vector2 getBoundsDX();
             virtual Vector2 getBoundsDY();
 
