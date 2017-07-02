c9c193b1f4f170d202f41ad5b2bd817cf148d4e6
schonmann <antonio_schonmann@hotmail.com>
2017-07-02 12:02:42 -0700

    Arduino hid joystick->keyboard mapping.

--- arduino/joystick.ino
+++ arduino/joystick.ino
@@ -1,9 +1,22 @@
-const int SW_pin = 2;
-const int Y_pin = 0;
-const int X_pin = 1;
-const int origin = 512;
+//***Assume arduino no modo HID***
 
+//Buffer para ativação das teclas.
+uint8_t buf[8] = { 0 };
+//Botão do analógico.
+const int SW_pin = 2;
+//Eixo Y.
+const int Y_pin = 0;
+//Eixo X.
+const int X_pin = 1;
+//Origem.
+const int origin = 512;
+//Frequência de atualização.
 const int HZ = 60;
+//Scancodes das teclas.
+#define KEY_UP 82
+#define KEY_RIGHT 79
+#define KEY_DOWN 81
+#define KEY_LEFT 80
 
 void setup() {
   pinMode(SW_pin, INPUT);
@@ -13,13 +26,35 @@ void setup() {
 
 void loop() {
   int sw = digitalRead(SW_pin);
-  //Serial.print("sw: ");
-  Serial.println(sw);
+  
   int x = analogRead(X_pin) - origin;
-  //Serial.print("x: ");
-  Serial.println(x);
+  if(x > 200) {
+    buf[2] = KEY_RIGHT;
+    Serial.write(buf, 8);
+    releaseKey();
+  } else if (x < -200) {
+    buf[2] = KEY_LEFT;
+    Serial.write(buf, 8);
+    releaseKey();
+  }
+  
   int y = analogRead(Y_pin) - origin;
-  //Serial.print("y: ");
-  Serial.println(y);
+  if(y > 200) {
+    buf[2] = KEY_UP;
+    Serial.write(buf, 8);
+    releaseKey();
+  } else if (y < -200) {
+    buf[2] = KEY_DOWN;
+    Serial.write(buf, 8);
+    releaseKey();
+  }
+  
   delay(1000/HZ);
+}
+
+void releaseKey() 
+{
+  buf[0] = 0;
+  buf[2] = 0;
+  Serial.write(buf, 8);
 }
